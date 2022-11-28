26e0036d15d22cdc4ef5c15e0e4300617497474a
Vincent Li (RIT Student) <vl5604@rit.edu>
2022-11-28 13:40:12 -0500

    fin

--- server/test/app.js
+++ server/test/app.js
@@ -1,1 +1,11 @@
 require('./test_config.js');
+require('chai').should();
+
+const app = require('../src/app.js');
+
+describe('server', () => {
+  it('should start up the server', (done) => {
+    app.should.not.be.an('undefined');
+    done();
+  });
+});
--- server/test/utils/index.js
+++ server/test/utils/index.js
@@ -1,5 +1,47 @@
-// const config = require('../test_config.js');
-// const utils = require('../../src/utils');
+const config = require('../test_config.js');
+const utils = require('../../src/utils');
 //
-// const { limits } = config;
+const { limits } = config;
 //
+
+describe('utils', () => {
+  describe('add', () => {
+    it('should return sum of two numbers', (done) => {
+      const sum = utils.add(5, 10);
+      sum.should.equal(15);
+      done();
+    });
+
+    it('should clamp to the max', (done) => {
+      const sum = utils.add(5, 10000);
+      sum.should.equal(limits.MAX);
+      done();
+    });
+
+    it('should clamp to the min', (done) => {
+      const sum = utils.add(-5, 1);
+      sum.should.equal(limits.MIN);
+      done();
+    });
+  });
+
+  describe('subtract', () => {
+    it('should return difference of two numbers', (done) => {
+      const diff = utils.sub(10, 5);
+      diff.should.equal(5);
+      done();
+    });
+
+    it('should clamp to the max', (done) => {
+      const sum = utils.add(20000, 5);
+      sum.should.equal(limits.MAX);
+      done();
+    });
+
+    it('should clamp to the min', (done) => {
+      const sum = utils.sub(5, 10000);
+      sum.should.equal(limits.MIN);
+      done();
+    });
+  });
+});
